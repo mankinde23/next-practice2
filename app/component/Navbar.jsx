50653abf4e6d7ec7c9d985504ff1576e027f91d1
mankinde23 <makinded12@gmail.com>
2024-01-16 19:25:03 +0100

    learnt how to approach having a component used everywhere like the navbar and footer to the layout component

---
+++ app/component/Navbar.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link href="/">dami</Link>
+      <Link href="/tickets">money</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
