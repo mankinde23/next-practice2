e6d9fa8f8e7e4b4bfeba740742f554a20852620b
mankinde23 <makinded12@gmail.com>
2024-01-17 12:07:03 +0100

    learnt how to use the loaing ui and suspense

---
+++ app/loading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <main className="text-center">
+      <h2 className="text-black">loading ...</h2>
+      <p>hopefully not for too long</p>
+    </main>
+  );
+}
