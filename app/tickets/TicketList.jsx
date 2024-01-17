dd38f559b7d3662c6a936379f17e119387d0325f
mankinde23 <makinded12@gmail.com>
2024-01-17 07:22:29 +0100

    set the revalidate of the data to 0 this chnages the getting the data from the cache and always fetching new data

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 async function getTicket() {
-  const res = await fetch("http://localhost:4000/tickets",{next:{revalidate:30}});
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: { revalidate: 0 },
+  });
   return res.json();
 }
 
@@ -10,14 +12,16 @@ export default async function TicketList() {
     <>
       {tickets.map((ticket) => (
         <div key={ticket.id} className="card my-5">
-            <h3>{ticket.title}</h3>
-            <p>{ticket.body.slice(0,200)}...</p>
-            <div className={`pill ${ticket.priority}`}>
-                {ticket.priority} priority
-            </div>
+          <h3>{ticket.title}</h3>
+          <p>{ticket.body.slice(0, 200)}...</p>
+          <div className={`pill ${ticket.priority}`}>
+            {ticket.priority} priority
+          </div>
         </div>
       ))}
-      {tickets.length === 0 &&( <p className="text-center">There are no open tickets, aya ya !</p> )}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open tickets, aya ya !</p>
+      )}
     </>
   );
 }
